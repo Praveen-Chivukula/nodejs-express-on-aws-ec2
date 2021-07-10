251f161e89606d135777988311a9d819e0ab33d0
Praveen-Chivukula <vpchivukula@keydabra.com>
2021-07-10 13:40:43 +0530

    'remote2'

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 2.0</p>');
 })
 
 app.get('/products', (req, res) => {
   res.send([
     {
-      productId: '101',
+      productId: '11',
       price: 200
     },
     {
